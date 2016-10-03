175f2bae4b3d67aee58063fd0554b149cd96c112
Pavel Kravtsov <pavelkr.msc@gmail.com>
2016-10-03 21:50:07 +0300

    added comments for MainWindow

--- absolute2048/absolute2048/MainWindow.xaml.cs
+++ absolute2048/absolute2048/MainWindow.xaml.cs
@@ -23,20 +23,24 @@ namespace absolute2048
             InitializeComponent();
         }
 
+		// initialization event
         protected override void OnInitialized(EventArgs e)
         {
             base.OnInitialized(e);
 
+			// window size determination
             double multiplier = Global.windowSizeModifier / (Global.widthX + Global.heightY);
             this.Width = Global.widthX * multiplier + 16.6;
             this.Height = Global.heightY * multiplier + 39.6;
             this.Background = Global.backgroundColor;
 
+			// grid size determination
             gameGrid.Height = this.Height - 39;
             gameGrid.Width = this.Width - 16;
             drawCenteredText(gameGrid, new Canvas(), "Press N to start new game");
         }
 
+		// on key down event
         private void keyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Right)
@@ -81,30 +85,43 @@ namespace absolute2048
             }
         }
 
-        private void iniNewField()
+		/// <summary>
+		/// Initialize new field (method on "N" key down)
+		/// </summary>
+		private void iniNewField()
         {
-            gameGrid.Children.Clear();
+			// window size determination
+			gameGrid.Children.Clear();
             double multiplier = Global.windowSizeModifier / (Global.widthX + Global.heightY);
             this.Width = Global.widthX * multiplier + 16.6;
             this.Height = Global.heightY * multiplier + 39.6;
 
-            gameGrid.Height = this.Height - 39;
+			// grid size determination
+			gameGrid.Height = this.Height - 39;
             gameGrid.Width = this.Width - 16;
 
+			// field initialization
             currentField = new Field();
 			drawFrame(gameGrid, currentField);
 		}
 
+		/// <summary>
+		/// Draw frame (after every step)
+		/// </summary>
 		private void drawFrame(Grid grid, Field field)
 		{
 			grid.Children.Clear();
-			//grid.Children.Remove(fieldGrid);
+			//grid.Children.Remove(fieldGrid);		// leaks of RAM somewhere
 			drawLayout(grid);
 			drawNumbers(grid, field);
 		}
 
-        private void drawLayout(Grid grid)
+		/// <summary>
+		///  Draw layout grid
+		/// </summary>
+		private void drawLayout(Grid grid)
         {
+			// draw vertical lines
 			for (int i = 0; i <= Global.widthX; i++)
 			{
 				Line currentLine = new Line();
@@ -114,12 +131,13 @@ namespace absolute2048
 				currentLine.Y2 = grid.Height;
 				currentLine.Stroke = Global.lineColor;
 				if (i == 0 || i == Global.widthX)
-					currentLine.StrokeThickness = 3;
+					currentLine.StrokeThickness = 3; // borders
 				else
 					currentLine.StrokeThickness = 2;
 				grid.Children.Add(currentLine);
 			}
 
+			// draw horizontal lines
 			for (int i = 0; i <= Global.heightY; i++)
 			{
 				Line currentLine = new Line();
@@ -129,21 +147,26 @@ namespace absolute2048
 				currentLine.Y2 = grid.Height / Global.heightY * i;
 				currentLine.Stroke = Global.lineColor;
 				if (i == 0 || i == Global.heightY)
-					currentLine.StrokeThickness = 3;
+					currentLine.StrokeThickness = 3; // borders
 				else
 					currentLine.StrokeThickness = 2;
 				grid.Children.Add(currentLine);
 			}
 		}
 
+		/// <summary>
+		/// Draw all numbers on grid
+		/// </summary>
 		private void drawNumbers(Grid grid, Field field)
 		{
+			// adding UniformGrid
 			grid.Children.Add(fieldGrid);
 			fieldGrid.Children.Clear();
 			fieldGrid.Background = null;
 			fieldGrid.Columns = Global.widthX;
 			fieldGrid.Rows = Global.heightY;
 
+			// filling cells
 			for (int i = 0; i < Global.heightY; i++)
 			{
 				for (int j = 0; j < Global.widthX; j++)
@@ -160,6 +183,9 @@ namespace absolute2048
 			}
 		}
 
+		/// <summary>
+		/// Draw single number of a cell
+		/// </summary>
 		private void drawNumber(Cell cell)
 		{
 			TextBox txt = new TextBox()
@@ -175,9 +201,14 @@ namespace absolute2048
 			};
 			txt.Text = cell.label;
 			txt.FontWeight = FontWeights.Bold;
+			//if (txt.Width > fieldGrid.Width / fieldGrid.Columns)		// does not work =(
+			//	txt.FontSize /= 2;
 			fieldGrid.Children.Add(txt);
 		}
 
+		/// <summary>
+		/// Draw centered unclickable message
+		/// </summary>
 		public static void drawCenteredText(Grid grid, Canvas canvas, string text)
         {
             try
